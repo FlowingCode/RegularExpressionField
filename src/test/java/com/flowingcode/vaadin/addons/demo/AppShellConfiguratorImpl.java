fe4dd48c09f33a3f8fcfb0ab1731115a0df8f43c
Paola De Bartolo <paola.debartolo@gmail.com>
2025-11-04 18:06:40 -0300

    feat(demo): add AppShellConfiguratorImpl
    Needed for demos compatibility with Vaadin 25.

---
+++ src/test/java/com/flowingcode/vaadin/addons/demo/AppShellConfiguratorImpl.java
@@ -0,0 +1,9 @@
+package com.flowingcode.vaadin.addons.demo;
+
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.theme.Theme;
+
+@Theme
+public class AppShellConfiguratorImpl implements AppShellConfigurator {
+
+}
